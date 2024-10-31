e5ea8dd564c3873ea15af01c3f06d8ef9ad6f941
Fams-tech <diawmamefama@gmail.com>
2024-10-31 23:02:03 +0000

    modif

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(
       ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
       credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+        exposedHeaders: ['set-cookie']
     })
 );
 app.use(cookieParser());
@@ -37,15 +39,26 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 // MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connexion à MongoDB réussie !");
-    app.listen(port, () => {
+    app.listen(port, '0.0.0.0', () => {
       console.log(`Serveur démarré sur le port ${port}`);
+      console.log('Variables d\'environnement chargées:', {
+        port: process.env.PORT,
+        mongoUri: process.env.MONGODB_URI ? 'Défini' : 'Non défini',
+        nodeEnv: process.env.NODE_ENV
+      });
     });
   })
   .catch((error) => {
     console.error("Connexion à MongoDB échouée :", error);
+    process.exit(1);  // Arrêter le serveur en cas d'erreur de connexion
   });
